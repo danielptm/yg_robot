1d05c9bd37ece52520504fc3f36e69ec84da9ad0
Daniel Tuttle <disclosureguide@gmail.com>
2025-08-25 14:54:27 -0700

    updates

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import time
 from adafruit_motorkit import MotorKit
+from motor.controls import Controls
 
-# Create a MotorKit object
-# By default, it uses the I2C address 0x60
-kit = MotorKit()
+controls = Controls()
 
-# Control motor 1 (connected to M1 terminals)
-#kit.motor1.throttle = 0.5  # Set speed to full forward (0.0 for stop, -1.0 for full backward)
-kit.motor1.throttle = -1
-kit.motor2.throttle = 0
-time.sleep(2)            # Run for 0.5 seconds
-#kit.motor1.throttle = 0.0  # Stop the motor
-kit.motor1.throttle = 0.0
-kit.motor2.throttle = 0.0
+controls.forward()
+controls.stop()
+controls.backward()
+controls.stop()
+controls.hard_right_90()
---
+++ motor/controls.py
@@ -0,0 +1,39 @@
+from adafruit_motorkit import MotorKit
+import time
+
+class Controls:
+    kit = MotorKit()
+
+    def __init__(self):
+        pass
+
+    def forward(self, duration: int):
+        if duration is None:
+            self.kit.motor1.throttle = -1
+            self.kit.motor2.throttle = -1
+            time.sleep(1)
+        else:
+            self.kit.motor1.throttle = -1
+            self.kit.motor2.throttle = -1
+            time.sleep(duration)
+
+    def backward(self, duration: int):
+        if duration is None:
+            self.kit.motor1.throttle = 1
+            self.kit.motor2.throttle = -1
+            time.sleep(1)
+        else:
+            self.kit.motor1.throttle = 1
+            self.kit.motor2.throttle = 1
+            time.sleep(duration)
+
+    def stop(self):
+        self.kit.motor1.throttle = 0
+        self.kit.motor2.throttle = 0
+        time.sleep(1)
+
+    def hard_right_90(self):
+        self.kit.motor1.throttle = -1
+        self.kit.motor2.throttle = 1
+        time.sleep(1)
+
