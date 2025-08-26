2ce4fc7dcf8dfd02dc5a71d91d27b6dec6ddd541
Daniel Tuttle <disclosureguide@gmail.com>
2025-08-26 11:51:12 -0700

    retrigger

--- main2.py
+++ main2.py
@@ -1,1 +1,22 @@
+from main import controls
+from motor.controls import Controls
+
 print("Starting user input mode...")
+from motor.controls import Controls
+controls = Controls()
+char = ''
+char = input("f:forward, b:backward, r:right, l:left,s:stop ... c:cancel program")
+while char != 'c':
+    char = input("f:forward, b:backward, r:right, l:left,s:stop ... c:cancel program")
+    if char == 'f':
+        controls.forward()
+    if char == 'b':
+        controls.backward()
+    if char == 'r':
+        controls.hard_right_90()
+    if char == "l":
+        controls.hard_left_90()
+    else:
+        controls.stop()
+
+controls.stop()
