ba89d3d9d425ab194f5315084f61f355b8103738
Daniel Tuttle <disclosureguide@gmail.com>
2025-08-26 16:23:27 -0700

    retrigger

--- main2.py
+++ main2.py
@@ -4,14 +4,16 @@ from pubsub import pub
 
 print("Starting user input mode...")
 
-controls = Controls()
-controls.start()
-pub.subscribe(controls.listener, "motor_topic")
 
 char = ''
 while char != 'c':
     print("f:forward, b:backward, r:right, l:left,s:stop ... c:cancel program")
     char = input("Input: ")
+    controls = Controls()
+    if controls.is_alive():
+        controls.join()
+    controls.start()
+    pub.subscribe(controls.listener, "motor_topic")
     pub.sendMessage("motor_topic", arg1=char, arg2=None)
 
 controls.stop_motor()
