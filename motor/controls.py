97d782cb4e39e9fc1d8ba3bd7332bde635850b4d
Daniel Tuttle <disclosureguide@gmail.com>
2025-09-25 15:47:57 -0700

    retrigger

--- motor/controls.py
+++ motor/controls.py
@@ -1,10 +1,19 @@
 
+import time
+import board
+import pwmio
+
 from adafruit_motorkit import MotorKit
+from adafruit_motor import servo
+
 import threading
 
 
 class Controls:
     kit = MotorKit()
+    pwm = pwmio.PWMOut(board.D5, duty_cycle=0, frequency=50)
+    my_servo = servo.Servo(pwm, min_pulse=1000, max_pulse=2000)
+
     thread = None
     keep_going = True
     def __init__(self, char):
@@ -28,6 +37,8 @@ class Controls:
             self.right()
         if char == "l":
             self.left()
+        if char == "u":
+            self.up()
         else:
             self.keep_going = False
             self.stop_motor()
@@ -65,6 +76,13 @@ class Controls:
             self.kit.motor2.throttle = 0.65
             self.kit.motor3.throttle = -0.65
             self.kit.motor4.throttle = 0.65
+
+    def up(self):
+        while True:
+            print("Sweeping from 0 to 180 degrees...")
+            for angle in range(0, 181, 5):  # Move from 0 to 180 in 5-degree increments
+                self.my_servo.angle = angle
+                time.sleep(0.05)  # Small delay for smooth movement
 
 
 
