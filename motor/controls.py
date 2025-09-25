8199caf6a0e79fee9d47ad527c5eca70c3333909
Daniel Tuttle <disclosureguide@gmail.com>
2025-09-25 16:00:41 -0700

    retrigger

--- motor/controls.py
+++ motor/controls.py
@@ -10,7 +10,7 @@ import threading
 
 class Controls:
     kit = MotorKit()
-    kit = ServoKit(channels=16)
+    servo = ServoKit(channels=16)
 
 
     thread = None
@@ -77,33 +77,34 @@ class Controls:
             self.kit.motor4.throttle = 0.65
 
     def up(self):
-        self.kit = ServoKit(channels=16)
+        self.servo = ServoKit(channels=16)
 
         # --- Standard Servo Control ---
         # Set servo 0 to 90 degrees
         print("Setting servo 0 to 90 degrees...")
-        self.kit.servo[0].angle = 90
+        self.servo.servo[0].angle = 90
         time.sleep(2)  # Wait for 2 seconds
 
         # Set servo 0 to 0 degrees
         print("Setting servo 0 to 0 degrees...")
-        self.kit.servo[0].angle = 0
+        self.servo.servo[0].angle = 0
         time.sleep(2)  # Wait for 2 seconds
 
         # --- Continuous Rotation Servo Control ---
         # Set continuous servo 1 to full speed forward
         print("Setting continuous servo 1 to full speed...")
-        self.kit.continuous_servo[1].throttle = 1.0
+        self.servo.continuous_servo[1].throttle = 1.0
         time.sleep(2)  # Run for 2 seconds
 
         # Set continuous servo 1 to full speed backward
         print("Setting continuous servo 1 to full speed backward...")
-        self.kit.continuous_servo[1].throttle = -1.0
+        self.servo.continuous_servo[1].throttle = -1.0
         time.sleep(2)  # Run for 2 seconds
 
         # Stop continuous servo 1
         print("Stopping continuous servo 1...")
-        self.kit.continuous_servo[1].throttle = 0.0
+        self.servo.continuous_servo[1].throttle = 0.0
+
 
 
 
