55879fa4a019e5eda96ec7f88aa205e651d8d2c3
Daniel Tuttle <disclosureguide@gmail.com>
2025-09-26 15:15:18 -0700

    retrigger

--- kill_motor.py
+++ kill_motor.py
@@ -1,5 +1,5 @@
-# from adafruit_motorkit import MotorKit
-#
-# kit = MotorKit()
-# self.kit.motor1.throttle = 0
-# self.kit.motor2.throttle = 0
+from main2 import controls
+from motor.controls import Controls as MotorControls
+
+controls = MotorControls()
+controls.stop()
